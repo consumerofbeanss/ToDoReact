f24769749c26e1a7df2674fef56e17810572c85e
consumerofbeanss <114371990+consumerofbeanss@users.noreply.github.com>
2024-03-05 00:06:34 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import {Component, useState} from 'react';
 import {getFirestore, collection, query, where, doc, setDoc, addDoc, getDocs} from 'firebase/firestore';
 import {FaCheck, FaPlus} from 'react-icons/fa';
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import { DropdownItem } from 'react-bootstrap';
+
 
 export  default class App extends Component {
 
@@ -82,6 +86,12 @@ export  default class App extends Component {
             <div className={'mt-5'}>
               <Form handleAdd={this.handleAddTasks}/>
             </div>
+
+            <hr/>
+
+            <div className={'row mt-3'}>
+              <DropDown title="Credits"/>
+            </div>
           </div>
         </div>
       </div>
@@ -134,5 +144,18 @@ function Form({handleAdd}) {
         </div>
       </div>
     </form>
+  );
+}
+
+function DropDown() {
+  return (
+    <DropdownButton id="dropdown-item-button" title="Credits">
+      <Dropdown.ItemText>Name: Ostein Vittorio Vellim</Dropdown.ItemText>
+      <Dropdown.ItemText>ID: 2602206783</Dropdown.ItemText>
+      <Dropdown.ItemText>Source: Code with Ross on YouTube</Dropdown.ItemText>
+      <Dropdown.Item as={DropdownItem} href="https://www.youtube.com/watch?v=88uQj9Ynzc4&pp=ygUdcmVhY3QganMgdG8gZG8gbGlzdCA4IG1pbnV0ZXM%3D">
+        View on YouTube
+      </Dropdown.Item>
+    </DropdownButton>
   );
 }
